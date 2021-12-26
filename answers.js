025cae395a580e34a66d736314addee6ecb8e3c2
Lucas <lucas2carlos@gmail.com>
2021-12-25 21:29:49 -0500

    Fizz Buzz answered

--- answers.js
+++ answers.js
@@ -17,7 +17,17 @@ for (let i = 0; i<= 200; i++){
 ////////////////////////////////
 // Fizz Buzz
 ////////////////////////////////
-
+for (let i = 0; i<= 100; i++){
+    if(i % 5 === 0 && i % 3 === 0){
+        console.log("FizzBuzz")
+    } else if(i % 5 === 0){
+        console.log("Buzz")
+    } else if( i % 3 === 0){
+        console.log("Fizz")
+    } else {
+        console.log(i)
+    }
+}
 
 ////////////////////////////////
 // Wild Wild Life
